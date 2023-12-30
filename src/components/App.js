3a40f8e1ad86b722decea35df218e566795b6456
CornFrog <thecornfrog@gmail.com>
2023-12-30 15:39:01 -0500

    fixed secret message

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,7 @@ const App = (props) => {
     secretMessage = <img className="secret" src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2F10xylIOSXWAMne%2F200.gif&f=1&nofb=1&ipt=0930ece6f11a0c9099545d6991d36ff62d4381c8cd9189397700632f6d896357&ipo=images'/> 
   }
   // if user selects all places 
-  if(visitedPlaceIDs.length === props.data.length){
+  if(visitedPlaceIDs.length === places.length){
     // show secreter message
     secretMessage = <img className="secret" src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2Fa1c3df8d2e62745ad0a3e791246b5c5e%2Ftenor.gif%3Fitemid%3D12198769&f=1&nofb=1&ipt=51f3b34df9ee54422311f39a668dec8bbc748ec4f573ebea50a90a3995ef4d12&ipo=images'/> 
   }
